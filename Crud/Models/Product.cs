0f7e31f5f7339bd2835bfe7c09a847e87238c6d1
Sarah <sarahmousa656@gmail.com>
2025-10-17 07:18:23 +0000

    add product model

---
+++ Crud/Models/Product.cs
@@ -0,0 +1,8 @@
+using System;
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
